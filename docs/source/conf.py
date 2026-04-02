171b86ee31b49a91ec2c8a83eb03623aeb70439e
Wolf Cukier <wcukier@uchicago.edu>
2026-04-02 14:50:36 -0500

    version fixes again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,14 +9,14 @@
 
 import os
 import sys
-from importlib.metadata import version
+from importlib.metadata import version as _get_version
 sys.path.insert(0, os.path.abspath('../../src/'))
 
 
 project = 'carmapy'
 copyright = '2025, Wolf Cukier'
 author = 'Wolf Cukier'
-release = version('carmapy')
+release = _get_version('carmapy')
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
