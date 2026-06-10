5f61baf615f1c93b0f660924fbd249ac55390b92
dbss3 <43238097+dbss3@users.noreply.github.com>
2026-06-10 16:26:37 -0500

    added miepython as alternate to pymiescatt to compute opacitites as its JIT mode is much faster than piemiescatt. Used it to generate SiO mie data

--- src/carmapy/mie.py
+++ src/carmapy/mie.py
@@ -1,7 +1,8 @@
 import numpy as np
 import PyMieScatt as ps
 import os
-
+os.environ["MIEPYTHON_USE_JIT"] = "1"  # Set to "0" to disable JIT
+import miepython as mp
 from scipy.interpolate import interp1d
 from .constants import gas_dict
 
@@ -17,12 +18,14 @@ def _load_cloud_indices(path):
     k_interp = interp1d(wavelengths, data[:, 2])
     return n_interp, k_interp
 
-def gen_mie_table(species: str,
+def gen_mie_table(name: str,
+                  species: str,
                   min_r: float = 1e-8,
                   ratio: float = 2,
                   n_r: int = 80,
                   lambdas : np.ndarray = defualt_lambdas, 
-                  indices_path : str = None):
+                  indices_path : str = None,
+                  mie_routine: str = "miepython"):
     """(Re-)generate mie tables for the specified species.  Note the units for
     the indices of refraction table has wavelength in microns, not cm.
 
@@ -47,7 +50,7 @@ def gen_mie_table(species: str,
         By default will use the carmapy default files
     """
     if indices_path is None:
-        indices_path = os.path.join(os.path.dirname(_SRC),
+        indices_path = os.path.join(_SRC,
                                     "mie_tables",
                                     "indices",
                                     gas_dict[species]["opacity_files"])
@@ -55,17 +58,39 @@ def gen_mie_table(species: str,
 
     rs = np.array([min_r * ratio ** ir for ir in range(n_r)])
 
-    with open(os.path.join(_SRC, "mie_tables", f"{species}_user.dat"), "w+") as f:
+    with open(os.path.join(_SRC, "mie_tables", f"{name}_user.dat"), "w+") as f:
         f.write("r[cm]\tλ[cm]\tQ_ext\tQ_sca\tg\n")
-        for ibin in range(n_r):
-            for il in range(len(lambdas)):
-                m = n_interp(lambdas[il]) + 1j*k_interp(lambdas[il])
+        
+        print('Starting Mie Generation')
+        if mie_routine == "PyMieScatt":
+            for ibin in range(n_r):
+                for il in range(len(lambdas)):
+                    print(f"{ibin}/{n_r}, {il}/{len(lambdas)}")
+                    m = n_interp(lambdas[il]) + 1j*k_interp(lambdas[il])
 
-                mie_res = ps.AutoMieQ(m,
-                                lambdas[il]*1e7,
-                                2*rs[ibin]*1e7,
-                                asDict=True)
-                f.write(f"{rs[ibin]}\t{lambdas[il]}\t{mie_res['Qext']}"
-                        f"\t{mie_res['Qsca']}\t{mie_res['g']}\n")
+                    mie_res = ps.AutoMieQ(m,
+                                    lambdas[il]*1e7,
+                                    2*rs[ibin]*1e7,
+                                    asDict=True)
+                    f.write(f"{rs[ibin]}\t{lambdas[il]}\t{mie_res['Qext']}"
+                            f"\t{mie_res['Qsca']}\t{mie_res['g']}\n")
 
-            f.flush()
+                f.flush()
+    
+        elif mie_routine == "miepython":
+            m = n_interp(lambdas) - 1j*k_interp(lambdas)
+            for ibin in range(n_r):
+                print(f"{ibin}/{n_r}")
+                x_array = 2 * np.pi * rs[ibin] / lambdas # both in same units
+                print(np.min(x_array), np.max(x_array))
+                print(np.min(lambdas), np.max(lambdas))
+                print(rs[ibin])
+            
+                qext, qsca, qback, g = mp.efficiencies_mx(m, x_array)  # Ignoring qback as it's unused
+        
+                print('Calc done, writing to file')
+                for il in range(len(lambdas)):
+                    f.write(f"{rs[ibin]}\t{lambdas[il]}\t{qext[il]}\t{qsca[il]}\t{g[il]}\n")
+        
+        else:
+            raise ValueError(f"Invalid Mie routine specified: {mie_routine}")
